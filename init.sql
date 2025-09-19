61426625cd5f291e0d30d900dff0448277b1b04e
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2025-09-19 15:30:04 +0300

    update database

--- init.sql
+++ init.sql
@@ -1,34 +1,28 @@
 CREATE TABLE
-  "questions" (
-    "id" UUID NOT NULL,
+  "notes" (
+    "id" BIGINT NOT NULL,
+    "author_id" BIGINT NOT NULL,
     "title" TEXT NOT NULL,
-    "text" TEXT NOT NULL,
-    "user_id" UUID NOT NULL,
-    "created_at" DATE NOT NULL,
-    "is_moderated" BOOLEAN NOT NULL DEFAULT FALSE,
-    "is_hidden" BOOLEAN NOT NULL DEFAULT FALSE
+    "text" TEXT NOT NULL DEFAULT '""',
+    "created_at" TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
   );
 
 CREATE TABLE
   "users" (
-    "id" UUID NOT NULL,
-    "short_name" VARCHAR(255) NOT NULL,
-    "name" VARCHAR(255) NOT NULL,
-    "surname" VARCHAR(255) NULL,
-    "role" VARCHAR(255) CHECK ("role" IN ('USER', 'MODER', 'ADMIN')) NOT NULL DEFAULT 'USER',
-    "position" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(255) NOT NULL
+    "id" BIGINT NOT NULL,
+    "short_name" TEXT NOT NULL,
+    "name" TEXT NOT NULL
   );
 
 -- MARK: primary key
-ALTER TABLE "questions" ADD PRIMARY KEY ("id");
-
 ALTER TABLE "users" ADD PRIMARY KEY ("id");
 
+ALTER TABLE "notes" ADD PRIMARY KEY ("id");
+
 -- MARK: foreign key
-ALTER TABLE "questions" ADD CONSTRAINT "questions_user_id_foreign" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE;
+ALTER TABLE "notes" ADD CONSTRAINT "notes_author_id_foreign" FOREIGN KEY ("author_id") REFERENCES "users" ("id");
 
 -- MARK: unique
-ALTER TABLE "users" ADD CONSTRAINT "users_short_name_unique" UNIQUE ("short_name");
+ALTER TABLE "notes" ADD CONSTRAINT "notes_title_unique" UNIQUE ("title");
 
-ALTER TABLE "users" ADD CONSTRAINT "users_email_unique" UNIQUE ("email");
+ALTER TABLE "users" ADD CONSTRAINT "users_short_name_unique" UNIQUE ("short_name");
